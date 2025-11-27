c29316f3ccfe64f90dd586870cd0dafaf8dcd37b
bjusty11 <justibruno10@gmail.com>
2025-11-27 10:57:53 -0300

    feat: Cria as rotas do controller com mock

--- src/main/java/com/example/Biblioteca/Book/BookController.java
+++ src/main/java/com/example/Biblioteca/Book/BookController.java
@@ -2,13 +2,33 @@ package com.example.Biblioteca.Book;
 
 import org.springframework.web.bind.annotation.*;
 
+//LOCALHOST:8080/adicionar
+
 @RestController
-@RequestMapping
+@RequestMapping("/livros")
 public class BookController {
 
-        @GetMapping("/boasVindas")
-        public String boasVindas(){
-            return "Essa é minha primeira mensagem nessa rota!";
+    // GET -- Mandar uma requisição para mostrar os livros
+    @GetMapping("/listar")
+    public String listarLivros() {
+        return "Livros encontrados: ";
+    }
 
-        }
+    // POST -- Mandar uma requisição para adicionar livro
+    @PostMapping("/adicionar")
+    public String adicionarLivro() {
+        return ("Livro adicionado com sucesso!");
+    }
+
+    // PUT -- Mandar uma requisição para alterar os livros
+    @PutMapping("/alterar")
+    public String alterarLivro() {
+        return ("Livro alterado com sucesso!");
+    }
+
+    // DELETE -- Mandar uma requisição para deletar os livros
+    @DeleteMapping("/devolucao")
+    public String deletarLivro() {
+        return ("Livro deletado com sucesso!");
+    }
 }
